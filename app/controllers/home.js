357f3b81d6aa8fb40070d73bbf4eca2fd494a48c
Johanny de Lucena Santos <johanny.santos@ccc.ufcg.edu.br>
2018-07-01 16:49:27 -0300

    Alguns comentarios explicativos acrescentados

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,6 +1,8 @@
 module.exports = function() {
     let controller = {};
 
+    //Funcao que renderiza a pagina index
+    //Funcao chamada em routes/home.js
     controller.index = function(req, res) {
         res.render('index', { nome: 'ArrumAqui' });
     };
--- app/routes/home.js
+++ app/routes/home.js
@@ -1,4 +1,7 @@
 module.exports = (app) => {
+    //Vai procurar diretamente na instancia do express
+    //passaa como parametro. Pasta app/controllers/home
     let controller = app.controllers.home;
+    //Funcao de controllers/home passada como parametro
     app.get('/', controller.index);
 }
