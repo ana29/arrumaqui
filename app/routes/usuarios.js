e66e6ff92467797d969e2054bed3f5adab329a46
liraop <pedrolira@live.com>
2018-07-09 21:31:10 -0300

    Refactoring routes

--- app/routes/usuarios.js
+++ app/routes/usuarios.js
@@ -4,15 +4,11 @@ module.exports = function(app) {
     let controller = app.controllers.usuarios;
     
     app.route('/api/usuarios')
-        .post(controller.salvaUsuario);
-
-    app.route('/api/usuarios')
+        .post(controller.salvaUsuario)
         .get(controller.listaTodos);
 
     app.route('/api/usuarios/:email')
-        .get(controller.obtemUsuarioComEmail);
-    
-    app.route('/api/usuarios/:email')
+        .get(controller.obtemUsuarioComEmail)
         .delete(controller.removeUsuario);
 
 }
