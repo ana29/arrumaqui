107e12ba5e34a709673686fb2210e3a611e1bc86
Ana <ana.godoy@ccc.ufcg.edu.br>
2018-07-09 21:03:54 -0300

    Atualizando CRUD de usuario

--- app/controllers/usuarios.js
+++ app/controllers/usuarios.js
@@ -1,63 +1,78 @@
 //Controller para a entidade usuario
 
-module.exports = function(app) {
+module.exports = function (app) {
     var Usuario = app.models.usuario;
+    var controller = {};
 
-    let controller = {};
-    
     //Função que salva o usuario no bd 
     controller.salvaUsuario = (req, res) => {
         console.log('API: salvaUsuario');
-    
         var usuario = new Usuario(req.body);
-    
         usuario.save(function (erro, usuario) {
-          if (erro) {
-            res.status(500).json(erro).end();
-            console.log(erro);
-          } else {
-            res.json(usuario);
-            res.status(201);
-            console.log(201);
+            if (erro) {
+                res.status(500).json(erro).end();
+                console.log(erro);
+            } else {
+                res.json(usuario);
+                res.status(201);
+                console.log(201);
 
-          }
+            }
         });
-    
-    
-      }
-
-      
 
 
-    //Função que retorna um usuario pelo ID
-    controller.obtemUsuario = function(req, res) {
-        let idUsuario = req.params.id;
-        let usuario = usuarios.filter(function(usuario) {
-            return usuario._id == idusuario;
-        })[0];
+    }
 
-        console.log('API: obtemUsuario');
+    //Função que lista Todos os usuarios do bd 
+    controller.listaTodos = function (req, res) {
+        Usuario.find().exec().then(
+            function (usuario) {
+                res.json(usuario);
+                res.status(201);
+                console.log(201);
+            },
+            function (erro) {
+                console.error(erro);
+                res.status(500).json(erro);
+            }
+        );
+    };
 
-        usuario ?
-            res.json(usuario) :
-            res.status(404).send('Contato não encontrado');
+
+    //Função que retorna um usuario pelo email
+    controller.obtemUsuarioComEmail = function (req, res) {
+        console.log('API: obtemUsuarioComEmail');
+        let _emailUsuario = req.params.email;
+        let criterio = { "email": _emailUsuario };
+        Usuario.find(criterio).exec()
+            .then(function (usuario) {
+                if (!usuario) throw new Error("Contato não encontrado");
+                res.json(usuario)
+                res.status(201);
+                console.log(usuario);
+            },
+                function (erro) {
+                    console.log(erro);
+                    res.status(404).json(erro);
+                }
+            );
     };
 
     //Função que remove um usuario
     controller.removeUsuario = (req, res) => {
         console.log('API: removeUsuario');
 
-        let idUsuario = req.params.id;
-
-        usuarios = usuarios.filter((usuario) => {
-            return usuario._id != idUsuario;
-        });
-
-        res.sendStatus(204).end();
-    }
-
- 
-
-
+        let _emailUsuario = req.params.email;
+        let criterio = { "email": _emailUsuario };
+        Usuario.remove(criterio).exec()
+            .then(
+                function () {
+                    res.end();
+                },
+                function (erro) {
+                    return console.error(erro);
+                }
+            );
+    };
     return controller;
 }
--- app/routes/usuarios.js
+++ app/routes/usuarios.js
@@ -6,7 +6,13 @@ module.exports = function(app) {
     app.route('/api/usuario')
         .post(controller.salvaUsuario);
 
+    app.route('/api/usuarios')
+        .get(controller.listaTodos);
 
-
+    app.route('/api/usuarios/:email')
+        .get(controller.obtemUsuarioComEmail);
+    
+    app.route('/api/usuarios/:email')
+        .delete(controller.removeUsuario);
 
 }
