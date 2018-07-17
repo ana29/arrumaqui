ed5e0a60fdf19dd86d06020ccf0d7e58505dfc28
Johanny de Lucena Santos <johanny.santos@ccc.ufcg.edu.br>
2018-07-17 03:54:51 -0300

    Nothing

--- public/js/services/CadastroService.js
+++ public/js/services/CadastroService.js
@@ -2,7 +2,7 @@ angular.module('arrumaqui')
 
 .factory('CadastroService', function($resource) {
 
-    //Cria o resource com o nome da rota configurada 
+    //Cria o resource com o nome da rota configurada
     //em app/routes/cadastrar.js
     return $resource('/api/usuarios');
 })
